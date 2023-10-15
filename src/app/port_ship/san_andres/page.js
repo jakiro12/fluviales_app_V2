c7cc6209c764a3d22949cef981673f2260ffee67
Jakiro12 <eclipsisnocte@gmail.com>
2023-10-14 23:09:47 -0300

    feat:nuevas imagenes y seleccion de parador dinamico

--- src/app/port_ship/san_andres/page.js
+++ src/app/port_ship/san_andres/page.js
@@ -2,28 +2,33 @@
 import styles from './styles.module.css';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-
+import { useState } from 'react';
 export default function GardenShip(){
     //seria lo recomendable tener una img de fondo y cambiar su url con useState
+    const [imgRef,setImgRef]=useState(1)
     const router = useRouter()
     const setDestiny=()=>{
         router.push('/port_ship/aviable_times')
+    }
+    const setSpotToTravel =(e)=>{
+        const value = Number(e.target.value);
+        setImgRef(value);
     }
     return(
         <article className={styles.spots_box}>
             <main className={styles.spot_card}>
                 <h3>¿A donde quiere ir?</h3>
                 <div className={styles.options_selected}>
-                    <select >
-                        <option>uno</option>
-                        <option>dos</option>
-                        <option>tre</option>
+                     <select  onChange={(e)=>setSpotToTravel(e)}>
+                        <option value={1}>uno</option>
+                        <option value={2}>dos</option>
+                        <option value={3}>tres</option>
                     </select>
                 </div>
                 <div className={styles.image_card} onClick={setDestiny}
                 style={{ position: 'relative', overflow: 'hidden' }}>
                         <Image
-                          src="/parador1.jpg"
+                          src={`/parador${imgRef}.jpg`}
                           fill={true}
                           alt="Boat to"
                           style={{objectFit:'cover'}}
